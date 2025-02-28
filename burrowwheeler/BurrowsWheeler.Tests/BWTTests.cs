f87d954aef87389885725d1b8fdf7173f4c103f6
stanislavgintovt <stanislavgintovt2004@gmail.com>
2025-02-28 15:16:11 +0300

    renamed BWTTest.cs

--- burrowwheeler/BurrowsWheeler.Tests/BWTTests.cs
+++ burrowwheeler/BurrowsWheeler.Tests/BWTTests.cs
@@ -2,12 +2,12 @@
 
 namespace BurrowsWheeler.Tests;
 
-public class Tests
+public class BWTTests
 {
     [TestCase("")] // empty string
     [TestCase("a")]
     [TestCase("banana")]
-    [TestCase(null)]
+    //[TestCase(null)]
     [TestCase("abracadabra")]
     [TestCase("So long string that I hope it will crash the program")]
     [TestCase("Soвыа long stфыriа!ы@вn4#g ^t2at%вы I' hвы:ope it ^wiывl234l c?rasывh/ tпheпы ыавы program но еsdasщё с русски\nми буквами и спец сим\"волами")]
