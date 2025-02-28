267e4fe6e359aa0f7088347b7d5ac25af5df2cc1
stanislavgintovt <stanislavgintovt2004@gmail.com>
2025-02-28 14:24:09 +0300

    Update UnitTest1.cs

--- burrowwheeler/BurrowsWheeler.Tests/UnitTest1.cs
+++ burrowwheeler/BurrowsWheeler.Tests/UnitTest1.cs
@@ -12,11 +12,15 @@ public class Tests
         _transform = new();
     }
 
-    [Test]
-    public void IsReverseBWTReturnsSourse()
+    [TestCase("")] // empty string
+    [TestCase("a")]
+    [TestCase("banana")]
+    [TestCase(null)]
+    [TestCase("abracadabra")]
+    [TestCase("So long string that I hope it will crash the program")]
+    [TestCase("Soвыа long stфыriа!ы@вn4#g ^t2at%вы I' hвы:ope it ^wiывl234l c?rasывh/ tпheпы ыавы program но еsdasщё с русски\nми буквами и спец сим\"волами")]
+    public void IsReverseBWTReturnsSourse(string? source)
     {
-        string source = "banana";
-
         (string res, int index) = Transformation.BurrowWheelerTransformation(source);
 
         Assert.That(Transformation.ReverseBurrowWheelerTransformation(res, index), Is.EqualTo(source));
