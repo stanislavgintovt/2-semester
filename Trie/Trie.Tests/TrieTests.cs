69b698611b2eca4ffa41b7be8d42d1ccfad93cd6
stanislavgintovt <stanislavgintovt2004@gmail.com>
2025-03-07 19:20:32 +0300

    Trie
    Структура данных типа "бор". По асимптотике все операции линейны, кроме поиска количества слов, начинающихся с определённого префикса, которая константна по сложности.
    
    Написаны тесты, проверяющие корректность работы операций и возможность ввода всех символов с клавиатуры.

---
+++ Trie/Trie.Tests/TrieTests.cs
@@ -0,0 +1,110 @@
+﻿using Trie;
+
+namespace Trie.Tests;
+
+public class Tests
+{
+    public string[] testcases = new string[20];
+    Trie trie;
+    [SetUp]
+    public void Setup()
+    {
+        trie = new();
+        testcases[0] = "he";
+        testcases[1] = "she";
+        testcases[2] = "his";
+        testcases[3] = "her";
+    }
+
+    [Test]
+    public void AddTest()
+    {
+        bool IsPassed = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (!trie.Add(testcases[i]))
+            {
+                IsPassed = false;
+            }
+        }
+        Assert.That(IsPassed);
+    }
+
+    [Test]
+    public void TwiceAddTest()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        bool IsPassed = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (trie.Add(testcases[i]))
+            {
+                IsPassed = false;
+            }
+        }
+        Assert.That(IsPassed);
+    }
+
+    [Test]
+    public void ContainTest()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        bool IsPassed = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (!trie.Contain(testcases[i]))
+            {
+                IsPassed = false;
+            }
+        }
+        Assert.That(IsPassed);
+    }
+    [Test]
+    public void PrefixCountTest()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        Assert.That(trie.HowManyStartsWithPrefix("s") == 1 && trie.HowManyStartsWithPrefix("h") == 3);
+    }
+    [Test]
+    public void RemoveTest()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        Assert.That(trie.Remove("he") && trie.HowManyStartsWithPrefix("h") == 2);
+    }
+    [Test]
+    public void RemoveTest2()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        Assert.That(trie.Remove("she") && trie.HowManyStartsWithPrefix("s") == 0);
+    }
+    [Test]
+    public void AllSymbolsTest()
+    {
+        Assert.That(trie.Add("`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,"));
+    }
+    [Test]
+    public void RemoveTest3()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            trie.Add(testcases[i]);
+        }
+        trie.Remove("he");
+        Assert.That(trie.Contain("her"));
+    }
+}
---
+++ Trie/Trie/Trie.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+
+namespace Trie
+{
+    public class Trie
+    {
+        Vertex head;
+        private int _size;
+        public int Size
+        {
+            get => _size;
+        }
+        public Trie()
+        {
+            head = new();
+        }
+
+        class Vertex
+        {
+            const int alphabethSize = 2048;
+            public Vertex?[] next;
+            public bool isTerminal;
+            public int size;
+
+           public Vertex()
+            {
+                next = new Vertex[alphabethSize];
+                isTerminal = false;
+                size = 0;
+            }
+
+            public Vertex? GetNext(char element)
+            {
+                return next[element];
+            }
+            public bool Add(char element)
+            {
+                if (next[element] == null)
+                {
+                    next[element] = new();
+                    size++;
+                    return true;
+                }
+                return false;
+            }
+
+            public bool Contain(char element)
+            {
+                if (next[element] != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public bool Remove(char element)
+            {
+                if (next[element] != null)
+                {
+                    next[element] = null;
+                    size--;
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool Contain(string element)
+        {
+            Vertex current = head;
+            for (int i = 0; i < element.Length; i++)
+            {
+                if (!current.Contain(element[i]))
+                {
+                    return false;
+                }
+                current = current.next[element[i]];
+            }
+            return current.isTerminal;
+        }
+
+        public bool Add(string element)
+        {
+            Vertex current = head;
+            if (Contain(element))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < element.Length; i++)
+            {
+                if (!current.Contain(element[i]))
+                {
+                    current.Add(element[i]);
+                }
+                else
+                {
+                    current.size++;
+                }
+                    current = current.next[element[i]];
+            }
+            current.isTerminal = true;
+            _size++;
+            return true;
+        }
+
+        public bool Remove(string element)
+        {
+            Vertex current = head;
+
+            if (!Contain(element))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < element.Length; i++)
+            {
+                if (current.next[element[i]].size == 1 && current.isTerminal)
+                {
+                    current.Remove(element[i]);
+                    return true;
+                }
+                current.size--;
+                current = current.next[element[i]];
+            }
+            if (current.isTerminal)
+            {
+                current.isTerminal = false;
+                current.size--;
+                return true;
+            }
+
+            throw new Exception("proga ne rabotaet");
+        }
+
+        public bool Add(string[] element)
+        {
+            bool flag = false;
+            foreach (string i in element)
+            {
+                if (!Add(i))
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        public int HowManyStartsWithPrefix(string prefix)
+        {
+            Vertex current = head;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                if (!current.Contain(prefix[i]))
+                {
+                    return 0;
+                }
+                current = current.next[prefix[i]];
+            }
+            return current.size;
+        }
+    }
+}
